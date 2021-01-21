0d07964ffb9733019b0a4bee99517d4032b4029b
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2021-01-21 21:43:52 +0100

    add documentation comments

--- src/client/resource-client/resource-client.interface.ts
+++ src/client/resource-client/resource-client.interface.ts
@@ -21,24 +21,73 @@ export type ResourceUpdateParams = {
   name?: string;
 };
 export interface IResourceClient {
+  /**
+   * public.resource.create
+   *
+   * Creates new Resource object, that can be used to create Slots and Bookings within it.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/create-a-resource
+   */
   create(
     params: Overbooked.ResourceCreateParams
   ): Promise<Response<Overbooked.Resource>>;
 
+  /**
+   * public.booking.get
+   *
+   * Retrieves a Resource details.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/get-a-resource
+   */
   get(id: string): Promise<Response<Overbooked.Resource>>;
 
+  /**
+   * public.resource.list
+   *
+   * Returns paginated list of all Resources matched by given filters.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/list-resources
+   */
   list(
     params: Overbooked.ResourceListParams
   ): Promise<PaginatedResponse<Overbooked.Resource[]>>;
 
+  /**
+   * public.resource.update
+   *
+   * Updates the specified Resource by setting the values of the parameters passed. Any parameters not provided will be left unchanged.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/update-a-resource
+   */
   update(
     id: string,
     params: ResourceUpdateParams
   ): Promise<Response<Overbooked.Resource>>;
 
+  /**
+   * public.resource.publish
+   *
+   * Changes the status from draft to published and makes all assigned Slots publicly visible.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/publish-a-resource
+   */
   publish(id: string): Promise<Response<Overbooked.Resource>>;
 
+  /**
+   * public.resource.convertToDraft
+   *
+   * Reverts the status from published to draft.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/convert-a-resource-to-draft
+   */
   convertToDraft(id: string): Promise<Response<Overbooked.Resource>>;
 
+  /**
+   * public.resource.delete
+   *
+   * Permanently deletes the Resource with all Slots and all Bookings within it.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/resource/delete-a-resource
+   */
   delete(id: string): Promise<Response<Record<string, never>>>;
 }
--- src/client/slot-client/slot-client.interface.ts
+++ src/client/slot-client/slot-client.interface.ts
@@ -37,6 +37,13 @@ export type SlotUnlockParams = {
 };
 
 export interface ISlotClient {
+  /**
+   * public.slot.create
+   *
+   * Creates new Slot objects, that can be used to create Bookings.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/create-slots
+   */
   create(
     params: Overbooked.SlotCreateParams
   ): Promise<Response<Overbooked.Slot[]>>;
@@ -44,25 +51,81 @@ export interface ISlotClient {
     params: Overbooked.SlotCreateParams[]
   ): Promise<Response<Overbooked.Slot[]>>;
 
+  /**
+   * public.slot.get
+   *
+   * Retrieves a Slot details.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/get-a-slot
+   */
   get(id: string): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.list
+   *
+   * Returns paginated list of all Slots matched by given filters.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/list-slots
+   */
   list(params: SlotListParams): Promise<PaginatedResponse<Overbooked.Slot[]>>;
 
+  /**
+   * public.slot.update
+   *
+   * Updates the specified Slot by setting the values of the parameters passed. Any parameters not provided will be left unchanged.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/update-a-slot
+   */
   update(
     id: string,
     params: SlotUpdateParams
   ): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.enable
+   *
+   * Changes the status from disabled to active and makes Slot publicly visible.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/enable-a-slot
+   */
   enable(id: string): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.disable
+   *
+   * Changes the status from active to disabled and makes Slot not visible.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/disable-a-slot
+   */
   disable(id: string): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.lock
+   *
+   * Locks the Slot for a while, making it impossible for others to book.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/lock-a-slot
+   */
   lock(id: string, params: SlotLockParams): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.unlock
+   *
+   * Unlocks the Slot making it available to book by others.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/unlock-a-slot
+   */
   unlock(
     id: string,
     params: SlotUnlockParams
   ): Promise<Response<Overbooked.Slot>>;
 
+  /**
+   * public.slot.delete
+   *
+   * Permanently deletes the Slot and all Bookings assigned to it.
+   *
+   * https://docs.overbooked.io/api-reference/api-resources/slot/delete-a-slot
+   */
   delete(id: string): Promise<Response<Record<string, never>>>;
 }
